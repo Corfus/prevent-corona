3ed16d6e8fc9e232eef38347e3987a82a3fd62e0
Christoph Holtmann <christoph.holtmann@live.de>
2020-03-21 16:25:19 +0100

    update ScientistsDieEvent for better balancing

--- src/gamelogic/events/ScientistsDieEvent.ts
+++ src/gamelogic/events/ScientistsDieEvent.ts
@@ -6,28 +6,29 @@ import { GameEventEntity } from '../GameEvent';
 
 export const ScientistsDieEntity: GameEventEntity = 'ScientistsDie';
 
-// Balancing
-export const OccursAboveMedicineChange: number = .5;
-export const ProbabilityAboveMedicineChange: number = .1;
-export const ProbabilityUnderMedicineChange: number = .03;
-export const HappinessChange: number = -10000;
-export const MedicineChangeMultiplier: number = .7;
-export const VaccinesChangeMultiplier: number = .7;
-
 export class ScientistsDieEvent extends LocalEvent {
+
+  // Balancing
+  private OccursAboveMedicineChange: number = .5;
+  private ProbabilityAboveMedicineChange: number = .1;
+  private ProbabilityUnderMedicineChange: number = .03;
+  private HappinessChangeAbsolute: number = -10000;
+  private MedicineChangeMultiplier: number = .7;
+  private VaccinesChangeMultiplier: number = .7;
+
   getLocalOccurenceProbability(state: GameState, countryEntity: string): number {
     const country = state.getCountry(countryEntity);
     const medicineChangeRate: number = country.medicine.absoluteRateOfChange;
     const vaccinesChangeRate: number = country.vaccines.absoluteRateOfChange;
-    return (medicineChangeRate > OccursAboveMedicineChange) ?
-      ProbabilityAboveMedicineChange : ProbabilityUnderMedicineChange;
+    return (medicineChangeRate > this.OccursAboveMedicineChange) ?
+      this.ProbabilityAboveMedicineChange : this.ProbabilityUnderMedicineChange;
   }
 
   occurLocally(state: GameState, countryEntity: CountryEntity): void {
     const country = state.getCountry(countryEntity);
-    country.happiness.value += HappinessChange;
-    country.medicine.absoluteRateOfChange *= MedicineChangeMultiplier;
-    country.vaccines.absoluteRateOfChange *= VaccinesChangeMultiplier;
+    country.happiness.value += this.HappinessChangeAbsolute;
+    country.medicine.absoluteRateOfChange *= this.MedicineChangeMultiplier;
+    country.vaccines.absoluteRateOfChange *= this.VaccinesChangeMultiplier;
     state.addEventMessage(new EventMessage(ScientistsDieEntity, countryEntity));
   }
 }
