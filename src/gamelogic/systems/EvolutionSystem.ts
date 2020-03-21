c28a969a7395de9383d12b66b02843938f9118e3
Leon Farchau <leon@makerspace-gt.de>
2020-03-21 14:53:16 +0100

    Grundstruktur für das Evolutionssystem eingeführt.
    Staatskapital, Zufriedenheit und Akzeptanz eingeführt

--- src/gamelogic/systems/EvolutionSystem.ts
+++ src/gamelogic/systems/EvolutionSystem.ts
@@ -3,9 +3,11 @@ import {GameState} from '../GameState';
 import {CountryEntity} from '../CountryState';
 
 export class EvolutionSystem extends System {
+  private happinessDeathsFactor: number;
+  private happinessInfectedFactor: number;
+  private acceptanceDeathsFactor: number;
+  private acceptanceInfectedFactor: number;
 
-  private initialEvolutionDone: boolean;
-  private initialEvolutionProbability: number;
   private rng: any; // TODO typing
 
   // Dependency injection für den RNG um testbar zu machen (brauchen wir das?)
@@ -15,44 +17,37 @@ export class EvolutionSystem extends System {
       rng = () => Math.random();
     }
     this.rng = rng;
-    this.initialEvolutionDone = false;
-    this.initialEvolutionProbability = initialEvolutionProbability;
+
+    //balancing Parameter
+    this.happinessDeathsFactor = 0.1;
+    this.happinessInfectedFactor = 0.01;
+    this.acceptanceDeathsFactor = 0.01;
+    this.acceptanceInfectedFactor = 0.001;
   }
 
-  private static infectCountry(state: GameState, countryEntity: CountryEntity): void {
-    console.log(`infecting ${countryEntity}`);
-    const country = state.getCountry(countryEntity);
-    country.numberOfInfected.value *= country.numberOfInfected.relativeRateOfChange;
-    country.numberOfInfected.value = Math.ceil(country.numberOfInfected.value); // aufrunden weil .4234 Infizierte nicht gehen
-    /** TODO sollte man eigentlich für alle CountryAttributes machen
-     * also "Evolution"-System und nicht Evolution System
-     * ABER? ist rate of change relativ(also *)oder absolut(+)?
-     */
-  }
 
   public applyTick(state: GameState): void {
-    if (!this.initialEvolutionDone) {
-      // schauen ob zufällig ein Virus ausbricht
-      if (this.rng() < this.initialEvolutionProbability) {
-        const countryEntities = state.getAllCountryEntities();
-        // TODO helper function?
-        const initialCountryEntity = countryEntities[Math.floor(this.rng() * countryEntities.length)];
-        // in diesem Land bricht der Virus aus
-        const initialCountry = state.getCountry(initialCountryEntity);
-        initialCountry.numberOfInfected.value = 1; // anfangs ein Infizierter
-        initialCountry.numberOfInfected.relativeRateOfChange = 1.1; // TODO sinnvoller default wert? ist das eigentlich Rate oder Faktor?
-        console.log(`infected ${initialCountryEntity}`);
-        this.initialEvolutionDone = true;
-      }
+    
 
-    } else {
-      console.log('Evolution spreads');
-      // Infektion verbreitet sich
-      const countries = state.getAllCountryEntities();
-      countries.forEach((v) => {
-        EvolutionSystem.infectCountry(state, v);
-      });
-    }
+    const countries = state.getAllCountryEntities();
+    countries.forEach((countryEntity) => {
+      const countryData = state.getCountry(countryEntity);
+
+      //Staatskapital
+      countryData.money.value += countryData.money.absoluteRateOfChange;
+
+      //Zufriedenheit
+      countryData.happiness.absoluteRateOfChange -= this.happinessDeathsFactor * countryData.deaths;
+      countryData.happiness.absoluteRateOfChange -= this.happinessInfectedFactor * countryData.numberOfInfected.value;
+      countryData.happiness.value += countryData.happiness.absoluteRateOfChange;
+
+      //Akzeptanz
+      var absoluteRateOfChange = countryData.numberOfInfected.value * this.acceptanceInfectedFactor;
+      absoluteRateOfChange += countryData.deaths * this.acceptanceDeathsFactor;
+      countryData.acceptance.value += absoluteRateOfChange;
+
+      
+    });
   }
 
 }
