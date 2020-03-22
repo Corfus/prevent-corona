22469801f4ed291465f38b5814175f61d1aa74b7
Jannik Berghoff <jannikberghoff@outlook.de>
2020-03-22 13:11:22 +0100

    Update action details pipe

--- src/app/pipes/action-details.pipe.ts
+++ src/app/pipes/action-details.pipe.ts
@@ -2,18 +2,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 
 const ACTION_ID_TO_TEXT: {[key: string]: string} = {
-  FakeNewsMessage: 'FakeNews_fromUs',
-  HygieneHandWashAdvice: 'Wash_Hands',
-  InformAboutPoliticanDeath: 'PoliticianDeath',
-  KeepDistanceAdvice: 'Distance',
-  LowDeathMessage: 'Propaganda',
-  MouthguardAdvide: 'MouthGuard',
-  EnactClosedBorder: 'Grenzschließung',
-  EnactClosedCompanies: 'Home_Office',
-  EnactClosedSchools: 'School',
-  EnactCurfew: 'Time',
-  EnactPayForMedicineDev: 'Medicine',
-  EnactPayForVaccineDev: 'Medicine'
+  tweet1: 'Fake-News werden verbreitet – Warnung! Im Netz gehen Fake News umher!',
+  tweet2: 'Hände waschen – Appell an alle Bürger: Bitte Hände oft und gründlich waschen.',
+  tweet3: 'Mundschutz tragen – Wer sich draußen aufhält, sollte bitte einen Mundschutz tragen, um sich und andere zu schützen!',
+  tweet4: 'Abstand halten – Wichtig! Haltet mindestens 1,5m Abstand zu anderen Personen! Noch besser: Nicht rausgehen :)',
+  tweet5: 'Geringe Todeszahlen – Deutschland hat immer noch niedrige Todeszahlen! Danke an alle, die sich an die Regelungen halten!',
+  tweet6: 'Politiker gestorben – Immer mehr Menschen sterben aufgrund des Viruses… So heute auch leider unser Bürgermeister aus Ferlensgart.',
+  tweet7: 'WirVsCorona Hackathon findet statt – Der WirvsVirus-Hackathon findet statt. 43000 nehmen teil und bleiben zuhause.',
+  tweet8: 'Corona Party – Eilmeldung! Eine Corona-Party hat stattgefunden und nun gibt es viele Neuinfizierte! ',
+  tweet9: 'Illegale Grenzüberschreitungen – Achtung! Illegale Grenzübergänge sind passiert! Bitte alle zuhause bleiben!',
+  tweet10: 'Wissenschaftler sterben – Danke für euren Einsatz! Leider sind schon mehrere Forscher am Virus gestorben… ',
+  tweet11: 'Aktienmarkt bricht ein – Neuigkeiten aus dem Aktienmarkt: Dieser ist heute eingebrochen.',
+  tweet12: 'Aktienmarkt bricht STARK ein – Börse aktuell: Der DAX bricht stark ein!',
+  tweet13: 'Aktienmarkt erholt sich – Erfreuliche News: Der Aktienmarkt erholt sich wieder!',
+  tweet14: 'China Reissack fällt um: Eilmeldung! In China ist der Reissack umgefallen.',
 };
 
 @Pipe({
