861455eef51328e50807b8ee1de020c97a49414f
Leon Farchau <leon@makerspace-gt.de>
2020-03-22 15:22:52 +0100

    Acceptance and happiness 0<x<100

--- src/gamelogic/GameCreator.ts
+++ src/gamelogic/GameCreator.ts
@@ -103,10 +103,10 @@ export class GameCreator {
     playerCountry.hospitalCapacityStartValue = 100000;
     playerCountry.hospitalCapacity = playerCountry.hospitalCapacityStartValue;
     playerCountry.happiness.value = 100;                                // 0 - 100        Start: 100
-    playerCountry.happiness.absoluteRateOfChange = -0.5;                // 
+    playerCountry.happiness.absoluteRateOfChange = -0.2;                // 
     playerCountry.numberOfInfected.value = 100;                           // 0 - 83.000.000 Start: 1
     playerCountry.numberOfInfected.relativeRateOfChange = 0.00000002;  // bitte behutsam ändern
-    playerCountry.money.value = 5000000000;                             // 0 - 50.000.000.000.000
+    playerCountry.money.value = 5000000000;                             // 0 - 5.000.000.000
     playerCountry.deathProbability.value = 0.03;                      // 0 - TODO
     playerCountry.recoverProbability.value = 0.005;                     // 0 - TODO
     playerCountry.acceptance.value = 0;                                 // 0 - 100        Start: 0
