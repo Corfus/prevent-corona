3ef0378e2fde4d0cb9e0078d21c2a0a6cf6c1dcc
Clemens du Bellier <clemens@ysaf.de>
2020-03-22 14:38:41 +0100

    css fix

--- src/app/components/sites/game-site/game-site.component.ts
+++ src/app/components/sites/game-site/game-site.component.ts
@@ -14,7 +14,6 @@ import {filter, map} from 'rxjs/operators';
 export class GameSiteComponent implements OnInit {
   actionSubject: Subject<GameActionEntity> = new Subject<GameActionEntity>();
   gameState$: Observable<GameState>;
-  enactedPolices$: Observable<Array<string>>;
   possibleActions$: Observable<Array<string>>;
   eventId$: Observable<string>;
 
